a2fd7c5d2b2fa1354d73d37c8a0e798b3648f702
Mohamed Athif N <149256653+A-THIF@users.noreply.github.com>
2026-04-11 12:48:19 +0530

    Add webhook route to sessionRoutes

--- src/routes/sessionRoutes.js
+++ src/routes/sessionRoutes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { createSession, joinSession, getSessionDetails } from '../controllers/sessionController.js';
+import { createSession, joinSession, getSessionDetails, handleAblyWebhook } from '../controllers/sessionController.js';
 const router = express.Router();
 
 router.post('/create', createSession);
 router.post('/join', joinSession);
 router.get('/:code', getSessionDetails);
+router.post('/webhook', handleAblyWebhook);
 
 export default router;
